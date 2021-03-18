4563b83aab04ac1004102ce8db75decfb3e8c485
Liza-T-XLS <shapi94@gmail.com>
2021-03-18 15:58:29 +0100

    CSS

--- app/views/home.tpl.php
+++ app/views/home.tpl.php
@@ -1,3 +1,4 @@
     <p>To discover the origin of this project log in with</p>
     <a class="OauthLink discord" href="https://discord.com/api/oauth2/authorize?client_id=<?= $_ENV['CLIENT_ID'] ?>&redirect_uri=http%3A%2F%2Flocalhost%3A8000%2Fconnect&response_type=code&scope=identify%20email" title="connect via Discord">Discord</a>
     <a class="OauthLink google" href="https://accounts.google.com/o/oauth2/v2/auth?scope=profile&access_type=online&response_type=code&redirect_uri=<?= urlencode('http://localhost:8000/connect-via-google')?>&client_id=<?= $_ENV['GOOGLE_CLIENT_ID'] ?>" title="connect via Google">Google</a>
+    <img class="homeImg" src="/assets/img/authentication.svg" alt="" />
