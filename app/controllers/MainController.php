7fa3155b1d91d3324c6736b74d204b25d6a384d1
Liza-T-XLS <shapi94@gmail.com>
2021-03-22 20:39:58 +0100

    Commented session cookie destruction due to public suffix list issue

--- app/controllers/MainController.php
+++ app/controllers/MainController.php
@@ -117,13 +117,14 @@ class MainController extends CoreController {
     // destroys the session's variables
     $_SESSION = array();
     // destroys session cookie
-    if (ini_get("session.use_cookies")) {
-        $params = session_get_cookie_params();
-        setcookie(session_name(), '', time() - 42000,
-            $params["path"], $params["domain"],
-            $params["secure"], $params["httponly"], $params["samesite"]
-        );
-    }
+    // if (ini_get("session.use_cookies")) {
+    //     $params = session_get_cookie_params();
+    //     setcookie(session_name(), '', time() - 42000,
+    //         $params["path"], $params["domain"],
+    //         $params["secure"], $params["httponly"], $params["samesite"]
+    //     );
+    // }
+    // <= not put to use in production due to public suffix list issue (https://devcenter.heroku.com/articles/cookies-and-herokuapp-com)
     session_destroy();
     $this->show('logout');
   }
